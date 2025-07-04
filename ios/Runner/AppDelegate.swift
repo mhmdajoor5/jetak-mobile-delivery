75e61892808951ab79efccfe6f02a9ff504ad291
Mohamed Adawy <meladawi@2p.com.sa>
2025-07-04 10:38:23 +0300

    update ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,8 @@
-import UIKit
 import Flutter
+import UIKit
 import Firebase
 import GoogleMaps
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,16 +10,13 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
     // Initialize Firebase
     FirebaseApp.configure()
 
     // Provide your Google Maps API Key
     GMSServices.provideAPIKey("YOUR_GOOGLE_MAPS_API_KEY")
-
-    // Register Flutter plugins
     GeneratedPluginRegistrant.register(with: self)
-
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
-import 'package:socket_io_client/socket_io_client.dart' as io;
-import 'package:socket_io_client/socket_io_client.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 import '../../generated/l10n.dart';
 import '../controllers/user_controller.dart';
@@ -16,7 +13,7 @@ class LoginWidget extends StatefulWidget {
 }
 
 class _LoginWidgetState extends StateMVC<LoginWidget> {
- late UserController _con;
+  late UserController _con;
 
   _LoginWidgetState() : super(UserController()) {
     _con = (controller as UserController?)!;
@@ -55,7 +52,7 @@ class _LoginWidgetState extends StateMVC<LoginWidget> {
                 height: config.App(context).appHeight(37),
                 child: Text(
                   S.of(context).lets_start_with_login,
-                  style: TextStyle(color: Colors.black54) ,
+                  style: TextStyle(color: Colors.black54),
                 ),
               ),
             ),
@@ -63,20 +60,19 @@ class _LoginWidgetState extends StateMVC<LoginWidget> {
               top: config.App(context).appHeight(37) - 50,
               child: Container(
                 decoration: BoxDecoration(
-                    color: Colors.black54,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 50,
-                        color: Theme.of(context).hintColor.withOpacity(0.2),
-                      )
-                    ]),
-                margin: EdgeInsets.symmetric(
-                  horizontal: 20,
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 50,
+                      color: Theme.of(context).hintColor.withOpacity(0.2),
+                    ),
+                  ],
                 ),
+                margin: EdgeInsets.symmetric(horizontal: 20),
                 padding: EdgeInsets.symmetric(vertical: 50, horizontal: 27),
                 width: config.App(context).appWidth(88),
-//              height: config.App(context).appHeight(55),
+                //              height: config.App(context).appHeight(55),
                 child: Form(
                   key: _con.loginFormKey,
                   child: Column(
@@ -86,58 +82,74 @@ class _LoginWidgetState extends StateMVC<LoginWidget> {
                       TextFormField(
                         keyboardType: TextInputType.emailAddress,
                         onSaved: (input) => _con.user.email = input!,
-                        validator: (input) => !input!.contains('@')
-                            ? S.of(context).should_be_a_valid_email
-                            : null,
+                        validator:
+                            (input) =>
+                                !input!.contains('@')
+                                    ? S.of(context).should_be_a_valid_email
+                                    : null,
                         decoration: InputDecoration(
                           labelText: S.of(context).email,
-                          labelStyle:
-                              TextStyle(color: Colors.black54),
+                          labelStyle: TextStyle(color: Colors.black54),
                           contentPadding: EdgeInsets.all(12),
                           hintText: 'johndoe@gmail.com',
                           hintStyle: TextStyle(
-                              color: Theme.of(context)
-                                  .focusColor
-                                  .withOpacity(0.7)),
-                          prefixIcon: Icon(Icons.alternate_email,
-                              color: Colors.black54),
+                            color: Theme.of(
+                              context,
+                            ).focusColor.withOpacity(0.7),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.alternate_email,
+                            color: Colors.black54,
+                          ),
                           border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context)
-                                      .focusColor
-                                      .withOpacity(0.2))),
+                            borderSide: BorderSide(
+                              color: Theme.of(
+                                context,
+                              ).focusColor.withOpacity(0.2),
+                            ),
+                          ),
                           focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context)
-                                      .focusColor
-                                      .withOpacity(0.5))),
+                            borderSide: BorderSide(
+                              color: Theme.of(
+                                context,
+                              ).focusColor.withOpacity(0.5),
+                            ),
+                          ),
                           enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context)
-                                      .focusColor
-                                      .withOpacity(0.2))),
+                            borderSide: BorderSide(
+                              color: Theme.of(
+                                context,
+                              ).focusColor.withOpacity(0.2),
+                            ),
+                          ),
                         ),
                       ),
                       SizedBox(height: 30),
                       TextFormField(
                         keyboardType: TextInputType.text,
                         onSaved: (input) => _con.user.password = input!,
-                        validator: (input) => input!.length < 3
-                            ? S.of(context).should_be_more_than_3_characters
-                            : null,
+                        validator:
+                            (input) =>
+                                input!.length < 3
+                                    ? S
+                                        .of(context)
+                                        .should_be_more_than_3_characters
+                                    : null,
                         obscureText: _con.hidePassword,
                         decoration: InputDecoration(
                           labelText: S.of(context).password,
-                          labelStyle:
-                              TextStyle(color: Colors.black54),
+                          labelStyle: TextStyle(color: Colors.black54),
                           contentPadding: EdgeInsets.all(12),
                           hintText: '••••••••••••',
                           hintStyle: TextStyle(
-                              color: Theme.of(context)
-                                  .focusColor
-                                  .withOpacity(0.7)),
-                          prefixIcon: Icon(Icons.lock_outline,
-                              color: Colors.black54),
+                            color: Theme.of(
+                              context,
+                            ).focusColor.withOpacity(0.7),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.lock_outline,
+                            color: Colors.black54,
+                          ),
                           suffixIcon: IconButton(
                             onPressed: () {
                               setState(() {
@@ -145,33 +157,40 @@ class _LoginWidgetState extends StateMVC<LoginWidget> {
                               });
                             },
                             color: Theme.of(context).focusColor,
-                            icon: Icon(_con.hidePassword
-                                ? Icons.visibility
-                                : Icons.visibility_off),
+                            icon: Icon(
+                              _con.hidePassword
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
                           ),
                           border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context)
-                                      .focusColor
-                                      .withOpacity(0.2))),
+                            borderSide: BorderSide(
+                              color: Theme.of(
+                                context,
+                              ).focusColor.withOpacity(0.2),
+                            ),
+                          ),
                           focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context)
-                                      .focusColor
-                                      .withOpacity(0.5))),
+                            borderSide: BorderSide(
+                              color: Theme.of(
+                                context,
+                              ).focusColor.withOpacity(0.5),
+                            ),
+                          ),
                           enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context)
-                                      .focusColor
-                                      .withOpacity(0.2))),
+                            borderSide: BorderSide(
+                              color: Theme.of(
+                                context,
+                              ).focusColor.withOpacity(0.2),
+                            ),
+                          ),
                         ),
                       ),
                       SizedBox(height: 30),
                       BlockButtonWidget(
                         text: Text(
                           S.of(context).login,
-                          style:
-                              TextStyle(color: Colors.black54),
+                          style: TextStyle(color: Colors.black54),
                         ),
                         color: Colors.black54,
                         onPressed: () {
@@ -193,8 +212,9 @@ class _LoginWidgetState extends StateMVC<LoginWidget> {
                     focusElevation: 0,
                     highlightElevation: 0,
                     onPressed: () {
-                      Navigator.of(context)
-                          .pushReplacementNamed('/ForgetPassword');
+                      Navigator.of(
+                        context,
+                      ).pushReplacementNamed('/ForgetPassword');
                     },
                     textColor: Theme.of(context).hintColor,
                     child: Text(S.of(context).i_forgot_password),
@@ -211,9 +231,10 @@ class _LoginWidgetState extends StateMVC<LoginWidget> {
                   ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
     );
-  }}
+  }
+}
